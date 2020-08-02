6e4f46cf9835ddeeda636b46e3addf4336e90de3
yk1979 <yukiirisawa1979@gmail.com>
2020-08-03 00:19:24 +0900

    master最新化 (#119)
    * Update dependency firebase to v7.17.1 (#106)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * typescript 3.9系対応 (#112)
    
    * Update dependency babel-plugin-styled-components to v1.11.1 (#111)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * Update typescript-eslint monorepo to v3.7.1 (#110)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * Update dependency eslint-plugin-react to v7.20.5 (#108)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * Update dependency react-redux to v7.2.1 (#107)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * Update dependency @babel/core to v7.11.0 (#113)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * Bump elliptic from 6.5.2 to 6.5.3 (#118)
    
    Bumps [elliptic](https://github.com/indutny/elliptic) from 6.5.2 to 6.5.3.
    - [Release notes](https://github.com/indutny/elliptic/releases)
    - [Commits](https://github.com/indutny/elliptic/compare/v6.5.2...v6.5.3)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Update dependency @types/styled-components to v5.1.2 (#115)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * Update dependency @types/react to v16.9.44 (#114)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * Update dependency ts-loader to v8.0.2 (#117)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * Update dependency eslint to v7.6.0 (#116)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,12 @@
-import { HYDRATE, MakeStore, createWrapper } from "next-redux-wrapper";
+import { ParsedUrlQuery } from "querystring";
+
+import { GetServerSideProps } from "next";
+import {
+  HYDRATE,
+  MakeStore,
+  MyNextContext,
+  createWrapper,
+} from "next-redux-wrapper";
 import {
   AnyAction,
   Store,
@@ -64,6 +72,17 @@ export const makeStore: MakeStore<RootState> = () => {
   return store;
 };
 
-export const wrapper = createWrapper<RootState>(makeStore, {
+const originalWrapper = createWrapper<RootState>(makeStore, {
   debug: process.env.NODE_ENV !== "production",
 });
+
+// typescript3.9以降で、next-redux-wrapper側で定義されているgetServerSideProps型が独自に拡張できなくなってしまった
+// contextをexpressに対応した独自の型に拡張するために以下のようにしている
+// next-redux-wrapperのアップデート状況に応じて見直す必要がある (thanks to @ics-ikeda)
+type MyWrapper = Omit<typeof originalWrapper, "getServerSideProps"> & {
+  getServerSideProps: <P extends Record<string, unknown> = any>(
+    callback: (context: MyNextContext) => void | P | Promise<P>
+  ) => GetServerSideProps<P, ParsedUrlQuery>;
+};
+
+export const wrapper = originalWrapper as MyWrapper;
